fd8fb367b7c23264db66f8b9845dc37c67e43a23
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-05-26 17:53:27 +0300

    resoudre erreur deploiement

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -41,7 +41,7 @@ export class AssignmentsService {
   }
 
   // renvoie un assignment par son id, renvoie undefined si pas trouvé
-  getAssignment(id: string): Observable<Assignment | undefined> {
+  getAssignment(id: number): Observable<Assignment | undefined> {
     const headers = AuthHeadersUtil.headers;
     return this.http.get<Assignment>(this.uri + '/' + id, { headers }).pipe(
       catchError(

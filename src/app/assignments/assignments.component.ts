e8a84fb5572890c2108e229e7baedf6d11b05376
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-05-28 20:41:46 +0300

    pagination

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -4,6 +4,7 @@ import { AssignmentsService } from '../shared/assignments.service';
 import { Assignment } from './assignment.model';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-assignments',
@@ -11,15 +12,20 @@ import { CommonModule } from '@angular/common';
   providers: [],
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.css'],
-  imports: [
-    CommonModule,
-    MatCardModule
-  ],
+  imports: [CommonModule, MatCardModule, MatPaginatorModule],
 })
 export class AssignmentsComponent implements OnInit {
-  assignments: Assignment[] = [];
+  titre = 'Liste des assignments';
   page = 1;
-  limit = 10;
+  limit = 2;
+  totalDocs!: number;
+  totalPages!: number;
+  nextPage!: number;
+  prevPage!: number;
+  hasNextPage!: boolean;
+  hasPrevPage!: boolean;
+  assignments: Assignment[] = [];
+  displayedColumns: string[] = ['nom', 'dateDeRendu', 'rendu'];
 
   @ViewChild('scroller') scroller!: CdkVirtualScrollViewport;
 
@@ -33,10 +39,28 @@ export class AssignmentsComponent implements OnInit {
   }
 
   getAssignmentsFromService() {
+    console.log("this.page", this.page);
+    console.log("this.limit", this.limit);
     this.assignmentsService
       .getAssignmentsPagines(this.page, this.limit)
       .subscribe((data) => {
+        console.log(
+          data.data
+        );
         this.assignments = data.data.docs;
+        this.totalDocs = data.data.totalDocs;
+        this.totalPages = data.data.totalPages;
+        this.nextPage = data.data.nextPage;
+        this.prevPage = data.data.prevPage;
+        this.hasNextPage = data.data.hasNextPage;
+        this.hasPrevPage = data.data.hasPrevPage;
       });
+  }
+
+  handlePageEvent(event: PageEvent) {
+    console.log(event);
+    this.page = event.pageIndex + 1;
+    this.limit = event.pageSize;
+    this.getAssignmentsFromService();
   }
 }

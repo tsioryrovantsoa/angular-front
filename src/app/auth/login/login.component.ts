662c781e56340c259eb326ea6e07c4f74f81267b
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-05-29 23:41:12 +0300

    resoudre css

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,12 +5,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule,
-    FormsModule,MatCardModule,MatFormFieldModule],
+    FormsModule,MatCardModule,MatFormFieldModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -18,14 +19,19 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private snackBar: MatSnackBar) {}
 
   onSubmit() {
     this.authService.logIn(this.username, this.password).subscribe(success => {
       if (success) {
+        this.snackBar.open('Connexion réussie ✔️', 'Fermer', {
+          duration: 3000
+        });
         this.router.navigate(['/home']);
       } else {
-        alert('Invalid credentials');
+        this.snackBar.open('Identifiants invalides ❌', 'Fermer', {
+          duration: 3000
+        });
       }
     });
   }

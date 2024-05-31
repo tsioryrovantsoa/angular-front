a46c4c9d6b327e48ff6674be3ac764ab97b36b8e
Tony Fenoaina <tfenoaina@gmail.com>
2024-05-31 22:08:53 +0300

    edit - suppression - noter only for admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { LoginGuard } from './shared//login.guard';
 import { AssignementOngletComponent } from './assignement-onglet/assignement-onglet.component';
 import { AssignementDragAndDropComponent } from './assignement-drag-and-drop/assignement-drag-and-drop.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AdminGuard } from './shared/admin.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
@@ -17,7 +18,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
   { path: "add", component: AddAssignmentComponent, canActivate: [AuthGuard] },
   { path: "assignments/:id", component: AssignmentDetailComponent, canActivate: [AuthGuard] },
-  { path: "assignments/:id/edit", component: EditAssignmentComponent, canActivate: [AuthGuard] },
+  { path: "assignments/:id/edit", component: EditAssignmentComponent, canActivate: [AuthGuard,AdminGuard] },
   { path: "matieres/:id", component: MatiereComponent, canActivate: [AuthGuard] },
   { path: 'onglets', component: AssignementOngletComponent, canActivate: [AuthGuard] },
   { path: 'drag-and-drop', component: AssignementDragAndDropComponent, canActivate: [AuthGuard] },
